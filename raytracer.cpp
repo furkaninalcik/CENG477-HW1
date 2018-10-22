cfd3933eadcb6f1ee7501aebd37a98f6ae726677
furkaninalcik <m.furkaninalcik@gmail.com>
2018-10-22 13:16:55 +0300

    started with image plane operations

--- raytracer.cpp
+++ raytracer.cpp
@@ -4,12 +4,89 @@
 
 typedef unsigned char RGB[3];
 
+using namespace parser;
+
+////////////////we may want to use struct instead of class
+
+class Ray{
+
+
+    private:
+        parser::Vec3f e;
+        parser::Vec3f d;
+
+    public:
+        
+        Ray(){
+            printf("empty ray constructor\n");
+        }
+        Ray(parser::Vec3f origin, parser::Vec3f direction ){
+
+            printf("ray constructor\n");
+
+
+            e = origin;
+            d = direction;
+        }
+        //~Ray();
+    
+};
+
+
 int main(int argc, char* argv[])
 {
     // Sample usage for reading an XML scene file
     parser::Scene scene;
 
     scene.loadFromXml(argv[1]);
+
+    Ray midEyeRay = Ray(scene.cameras[0].position , scene.cameras[0].gaze);
+
+    int n_x = scene.cameras[0].image_width;
+    int n_y = scene.cameras[0].image_height;
+
+    int l = scene.cameras[0].near_plane.x;
+    int r = scene.cameras[0].near_plane.y;
+    int b = scene.cameras[0].near_plane.z;
+    int t = scene.cameras[0].near_plane.w;
+
+    printf("width: %d \n"  , n_x);
+    printf("height: %d \n" , n_y);
+
+
+    //Ray RayArray[800*800] ;
+
+    printf("test\n");
+
+    for (int i = 0; i < n_x; ++i)
+    {
+        for (int j = 0; j < n_y; ++j)
+        {
+            // s_u = (r – l)(i + 0.5)/n_x
+            // s_v = (t – b)(j + 0.5)/n_y
+
+
+            printf("s_u: %lf \n" , (r - l)*(i + 0.5)/n_x );
+            printf("s_v: %lf \n" , (t - b)*(j + 0.5)/n_y );
+        }
+    }
+
+    printf("TOP LEFT  :\n"    );
+    printf("s_u: %lf \n" , (r - l)*(0 + 0.5)/n_x );
+    printf("s_v: %lf \n" , (t - b)*(0 + 0.5)/n_y );
+
+    printf("BOTTOM RIGHT  :\n"    );
+    printf("s_u: %lf \n" , (r - l)*(n_x-1 + 0.5)/n_x );
+    printf("s_v: %lf \n" , (t - b)*(n_y-1 + 0.5)/n_y );
+
+    printf("NUMBERS: \n");
+    printf("l: %d \n"  , l);
+    printf("r: %d \n"  , r);
+    printf("b: %d \n"  , b);
+    printf("t: %d \n"  , t);
+    printf("n_x: %d \n"  , n_x);
+    printf("n_y: %d \n"  , n_y);
+
 
     // The code below creates a test pattern and writes
     // it to a PPM file to demonstrate the usage of the
